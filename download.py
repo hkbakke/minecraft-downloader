009ae11de7e14b806f5ded37f1aa295f018b62bb
Hans-Kristian Bakke <hkbakke@gmail.com>
2018-11-29 20:21:45 +0100

    Add checksum verification and version selection

--- download.py
+++ download.py
@@ -5,6 +5,8 @@ from urllib.request import Request, urlopen
 import json
 import sys
 import logging
+import hashlib
+import os
 
 
 MANIFEST_URL = 'https://launchermeta.mojang.com/mc/game/version_manifest.json'
@@ -69,10 +71,26 @@ def download_file(url, filename=None):
     with open(filename, 'wb') as f:
         f.write(response.read())
 
+    return filename
+
+def get_checksum(filename):
+    h = hashlib.sha1()
+    with open(filename, 'rb') as f:
+        h.update(f.read())
+    return h.hexdigest()
+
+def verify(filename, checksum):
+    if get_checksum(filename) == checksum:
+        return True
+    return False
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--verbose', '-v', action='store_true', help='verbose output')
     parser.add_argument('--quiet', '-q', action='store_true', help='only output warnings and errors')
+    parser.add_argument('--filename', metavar='FILENAME', help='set output filename')
+    parser.add_argument('--version', metavar='VERSION', help='specify version if latest is not wanted')
+    parser.add_argument('--versioned-filename', action='store_true', help='put version in filename')
     args = parser.parse_args()
 
     if args.verbose:
@@ -87,8 +105,21 @@ def main():
     LOGGER.addHandler(console)
 
     manifest = Manifest()
-    package = manifest.get_package()
-    download_file(package.server_download_url)
+    package = manifest.get_package(args.version)
+    filename = None
+
+    if args.versioned_filename:
+        filename = 'server-%s.jar' % package.version
+    elif args.filename:
+        filename = args.filename
+
+    filename = download_file(package.server_download_url, filename)
+    
+    if not verify(filename, package.server_download_sha1):
+        LOGGER.info("Invalid checksum for '%s'. Removing...", filename)
+        os.unlink(filename)
+
+    LOGGER.info("Downloaded '%s'", filename)
 
 
 if __name__ == '__main__':
